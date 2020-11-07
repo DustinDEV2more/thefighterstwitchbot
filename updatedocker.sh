8ff237d50a1a3820a9092fc568ce22f2765b3a87
DustinDEV2more <dustin@dustin-dm.de>
2020-11-08 00:14:41 +0100

    update docker build sh

--- updatedocker.sh
+++ updatedocker.sh
@@ -2,4 +2,4 @@ git fetch
 git pull
 docker build -t dustin/fight-bot .
 docker rm --force Fight-Bot
-docker run -d --restart always --cap-add=SYS_ADMIN --name DM-Fight-Bot -p 6787:6787 dustin/fight-bot
+docker run -d --restart always --cap-add=SYS_ADMIN --name Fight-Bot -p 4536:4536 dustin/fight-bot
