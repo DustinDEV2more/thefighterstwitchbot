1fbefe18bbc0935271e8aee3e6b7bfd2a38db958
DustinDEV2more <dustin@dustin-dm.de>
2020-11-15 12:06:53 +0100

    fix welcome msg

--- events/welcome.js
+++ events/welcome.js
@@ -3,20 +3,22 @@ var {MessageEmbed} = require("discord.js");
 var MEMBER = require("../models/MEMBER");
 
 client.on("guildMemberAdd", async (member) => {
-   var joinmessage = client.channels.cache.get("771431807617990678").send(new MessageEmbed().setColor("RANDOM").setDescription(`<:join:723485426144378913> **${member.user.tag}** ist gerade den Kämpfern beigetreten`))
    var fetchmembers = await MEMBER.find({"id": member.id}) 
+
    if (fetchmembers.length > 0){
-       joinmessage.edit(new MessageEmbed().setColor("RANDOM").setDescription(`<:rejoin:723487019241701477> **${member.user.tag}** ist gerade erneut den Kämpfern beigetreten`))
-       MEMBER.findOneAndUpdate({"id": member.id}, {"lefted": null})
+      client.channels.cache.get("771431807617990678").send(new MessageEmbed().setColor("RANDOM").setDescription(`<:rejoin:723487019241701477> **${member.user.tag}** ist gerade erneut den Kämpfern beigetreten`))
+     await MEMBER.findOneAndUpdate({"id": member.id}, {"lefted": null})
     }
+
    else {
-    await new MEMBER({"id": member.id}).save()
+   var joinmessage = client.channels.cache.get("771431807617990678").send(new MessageEmbed().setColor("RANDOM").setDescription(`<:join:723485426144378913> **${member.user.tag}** ist gerade den Kämpfern beigetreten`))
+   await new MEMBER({"id": member.id}).save()
    }
     
 })
 
-client.on("guildMemberRemove", (member) => {
+client.on("guildMemberRemove", async (member) => {
    var leavemessage = client.channels.cache.get("771431807617990678").send(new MessageEmbed().setColor("RANDOM").setDescription(`<:leafe:723485426169413686> **${member.user.tag}** wurde vom Killer aufgehangen. Bye Bye`))
-   MEMBER.findOneAndUpdate({"id": member.id}, {"lefted": Date.now()})
+   await MEMBER.findOneAndUpdate({"id": member.id}, {"lefted": Date.now()})
    
 })
--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const client = new Discord.Client();
 exports.client = client;
 const mongoose = require("mongoose")
 
-mongoose.connect(config.mongo, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(config.mongo, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true}).then(() => {
     console.log("Database conncetion succsessful")
 })
 
 client.on("ready", () => {
     console.log(client.user.username + " is now online")
 })
-
 
 require("./webserver/webmain")
 require("./events/welcome")
